82d86cc55cf5ad333fade1603355fea122948f6f
yusufziyaozbay <yusufziyaozbay@gmail.com>
2023-07-28 14:47:16 +0300

    View options added.

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -5,6 +5,7 @@ class Bookmarks(models.Model):
     title = models.CharField(max_length=200, blank=False)
     url = models.URLField(max_length=200, blank=False)
     description = models.TextField()
+    image = models.URLField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect #, get_object_or_404
 
 from .models import *
+from django.contrib import messages
 # import django users:
 from django.contrib.auth.models import User
+# For link previews:
+import requests
+from bs4 import BeautifulSoup
 
 
 # Create your views here.
@@ -11,21 +15,32 @@ def index(request):
     try:
         bookmarks = Bookmarks.objects.filter(user=request.user)
     except:
-        return redirect('auth_index')
+        return redirect('auth_index') # DIKKATT
     
-    return render(request, 'bookmarks/index.html', {"bookmarks":bookmarks})
+    if not bookmarks:
+        messages.info(request, 'There are no records yet :/')
+    
+    return render(request, 'bookmarks/index.html', {'bookmarks' : bookmarks})
 
 
 def add(request):
     if request.method == 'POST':
         url = request.POST['url']
-        title = request.POST['title']
-        description = request.POST['description']
+        og_data = get_og_data(url)
+        if og_data == '':
+            return redirect('index')
+
+        # title = request.POST['title']
+        # description = request.POST['description']
+        
+        title = og_data['title']
+        description = og_data['description']
+        image = og_data['image']
         user = request.user
 
-        print(url, title, description, user)
+        print(url, title, description, user, image)
         
-        bookmark = Bookmarks(user=user, url=url, title=title, description=description)
+        bookmark = Bookmarks(user=user, url=url, title=title, description=description, image=image)
         bookmark.save()
         return redirect('/home')
     
@@ -49,3 +64,25 @@ def delete(request, id):
         bookmark = Bookmarks.objects.get(id=id)
         bookmark.delete()
     return redirect('/home')
+
+
+def get_og_data(url):
+    try:
+        response = requests.get(url)
+    except:
+        print('an error has occurred :/')
+        og_data = ''
+        return og_data
+    soup = BeautifulSoup(response.text, 'html.parser')
+    og_data = {
+        'title': get_og_meta(soup, 'og:title'),
+        'description': get_og_meta(soup, 'og:description'),
+        'image': get_og_meta(soup, 'og:image'),
+        # 'url': get_og_meta(soup, 'og:url'),
+    }
+    return og_data
+
+
+def get_og_meta(soup, property_name):
+    meta_tag = soup.find('meta', property=property_name)
+    return meta_tag['content'] if meta_tag else ''
